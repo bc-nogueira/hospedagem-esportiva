73c48f6eb818be5a617a103f0f632e446a809dfb
bcnogueira <bcnogueira@id.uff.br>
2017-05-03 03:12:55 -0300

    Criando e alterando classes de testes.

---
+++ src/java/teste/TesteCriaAvaliacao.java
@@ -0,0 +1,31 @@
+package teste;
+
+import java.util.Calendar;
+import javax.persistence.EntityManager;
+import model.Avaliacao;
+import model.TipoAvaliacao;
+import model.Usuario;
+import util.JpaUtil;
+
+public class TesteCriaAvaliacao {
+    public static void main(String[] args) {
+        Avaliacao avaliacao = new Avaliacao();
+        avaliacao.setDescricao("Bla bla bla.");
+        avaliacao.setDataAvaliacao(Calendar.getInstance());
+        avaliacao.setTipoAvaliacao(TipoAvaliacao.Usuario);
+        
+        EntityManager em = new JpaUtil().getEntityManager();
+        em.getTransaction().begin();
+        
+        Usuario avaliado = em.find(Usuario.class, 1);
+        Usuario avaliador = em.find(Usuario.class, 2);
+        
+        avaliacao.setAvaliado(avaliado);
+        avaliacao.setAvaliador(avaliador);
+        
+        em.persist(avaliacao);
+        
+        em.getTransaction().commit();
+        em.close();
+    }
+}
--- src/java/teste/TesteCriaUsuario.java
+++ src/java/teste/TesteCriaUsuario.java
@@ -7,18 +7,30 @@ import util.JpaUtil;
 public class TesteCriaUsuario {
 
     public static void main(String[] args) {
-        Usuario usuario = new Usuario();
-        usuario.setNome("Talita");
-        usuario.setSobrenome("Rezende");
-        usuario.setLocalMoradia("Maricá");
-        usuario.setEsporteFavorito("Futebol");
-        usuario.setDispostoReceber(Boolean.TRUE);
-        usuario.setQuantReceber(2);
+        Usuario usuario1 = new Usuario();
+        usuario1.setNome("Talita");
+        usuario1.setSobrenome("Rezende");
+        usuario1.setLocalMoradia("Maricá");
+        usuario1.setEsporteFavorito("Futebol");
+        usuario1.setDispostoReceber(Boolean.TRUE);
+        usuario1.setQuantReceber(2);
+        usuario1.setEmail("talita@gmail.com");
+        usuario1.setSenha("1234");
+        
+        Usuario usuario2 = new Usuario();
+        usuario2.setNome("Breno");
+        usuario2.setSobrenome("Nogueira");
+        usuario2.setLocalMoradia("Niterói");
+        usuario2.setEsporteFavorito("Futebol Americano");
+        usuario2.setDispostoReceber(Boolean.FALSE);
+        usuario2.setEmail("breno@gmail.com");
+        usuario2.setSenha("1234");
         
         EntityManager em = new JpaUtil().getEntityManager();
         em.getTransaction().begin();
         
-        em.persist(usuario);
+        em.persist(usuario1);
+        em.persist(usuario2);
         
         em.getTransaction().commit();
         em.close();
