b01f80114911cb6b667c861da3c5d0b0d8227bcf
bcnogueira <bcnogueira@id.uff.br>
2017-05-03 23:27:01 -0300

    Acertando relacionamento.

--- src/java/model/Avaliacao.java
+++ src/java/model/Avaliacao.java
@@ -9,17 +9,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "buscaRecebidaPorUsuarioETipo",
+            query = "select a from Avaliacao a " + 
+                    "where a.avaliado = :pAvaliado and a.tipoAvaliacao = :pTipo")
+})
 public class Avaliacao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Enum tipoAvaliacao;
+    private TipoAvaliacao tipoAvaliacao;
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false)
     private Calendar dataAvaliacao; //ou Date?
@@ -40,11 +47,11 @@ public class Avaliacao {
         this.id = id;
     }
 
-    public Enum getTipoAvaliacao() {
+    public TipoAvaliacao getTipoAvaliacao() {
         return tipoAvaliacao;
     }
 
-    public void setTipoAvaliacao(Enum tipoAvaliacao) {
+    public void setTipoAvaliacao(TipoAvaliacao tipoAvaliacao) {
         this.tipoAvaliacao = tipoAvaliacao;
     }
 
